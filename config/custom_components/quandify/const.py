c8364f8715cd46d01b283cf34059ded5398539f6
johanfolin <84206188+johanfolin@users.noreply.github.com>
2025-08-29 14:11:03 +0200

    Create const.py

---
+++ config/custom_components/quandify/const.py
@@ -0,0 +1,11 @@
+"""Constants for the Quandify integration."""
+
+DOMAIN = "quandify"
+DEFAULT_NAME = "Quandify Water"
+
+# API URLs
+AUTH_URL = "https://auth.quandify.com/"
+BASE_URL = "https://api.quandify.com"
+
+# Update intervals
+UPDATE_INTERVAL_MINUTES = 60  # Update every hour
